c995f6eaec06a9e830c2599563216f59f1f89d45
tamccullough88 <tamccullough88@gmail.com>
2023-11-27 19:44:23 -0500

    adding packages to packages page

--- react-router/src/components/Packages.js
+++ react-router/src/components/Packages.js
@@ -1,14 +1,18 @@
 
 
-export default function Packages() {
+export default function Packages({ packages }) {
+    const display = packages.map((spaPackages, i) => {
+        return <li key={i}>{spaPackages}</li>
+    })
 
     return (
         <div>
             <div className="packages">
                 <div className="packagesHeader">
                     <h1>Our Packages</h1>
+                    {display}
                 </div>
-                
+
             </div>
         </div>
     )
