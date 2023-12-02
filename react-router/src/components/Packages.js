58996519c552011609c137cbb42ac39638ec3b90
tamccullough88 <tamccullough88@gmail.com>
2023-12-02 10:42:40 -0500

    updated styling on about and packages pages

--- react-router/src/components/Packages.js
+++ react-router/src/components/Packages.js
@@ -1,19 +1,25 @@
+import Card from 'react-bootstrap/Card'
+import Container from 'react-bootstrap/Container'
+import ListGroup from 'react-bootstrap/ListGroup'
 
 
-export default function Packages({ packages }) {
-    const display = packages.map((spaPackages, i) => {
-        return <li key={i}>{spaPackages}</li>
-    })
+export default function Packages(props) {
+    const displayPackages = props.packages.map((eachPackage) =>
+        <ListGroup.Item>{eachPackage}</ListGroup.Item>)
 
     return (
-        <div>
-            <div className="packages">
-                <div className="packagesHeader">
-                    <h1>Our Packages</h1>
-                    {display}
-                </div>
-
-            </div>
-        </div>
+        <Container>
+            <Card>
+                <Card.Body>
+                    <Card.Title>Our Packages</Card.Title>
+                    <Card.Text>
+                        Check out some of our packages! Every package is ethically sourced and organic!
+                    </Card.Text>
+                </Card.Body>
+                <ListGroup>
+                    {displayPackages}
+                </ListGroup>
+            </Card>
+        </Container>
     )
 }
